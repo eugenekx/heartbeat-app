2d757113632cbeb565e4a81f916871e03dd7c8c1
eugenekx <kx.eugene@gmail.com>
2020-03-09 20:27:05 +0300

    HB-21 HB-9 closed

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Menu from './components/Menu';
 import AppNavbar from './components/AppNavbar';
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHome, faUser, faSignOutAlt, faMusic, faStar, faHistory, faHeartbeat, faBell, faPlay, faForward} from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faSignOutAlt, faMusic, faStar, faHistory, faHeartbeat, faBell, faPlay, faForward, faThumbsUp, faThumbsDown} from '@fortawesome/free-solid-svg-icons'
 import { faBandcamp, faSpotify, faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 import { Container, Row, Col } from 'reactstrap';
@@ -14,8 +14,11 @@ import FooterPlayer from './components/FooterPlayer';
 import GenresList from './components/GenresList';
 import SongPlayer from './components/SongPlayer';
 import ArtistInfo from './components/ArtistInfo';
+import YourRating from './components/YourRating';
 
-library.add(faHome, faUser, faSignOutAlt, faMusic, faStar, faHistory, faHeartbeat, faBell, faPlay, faForward, faBandcamp, faSpotify, faFacebook, faTwitter);
+
+library.add(faHome, faUser, faSignOutAlt, faMusic, faStar, faHistory, faHeartbeat, faBell, faPlay, faForward, faBandcamp, faSpotify, faFacebook, faTwitter, 
+            faThumbsUp, faThumbsDown);
 
 function App() {
   return (
@@ -30,7 +33,16 @@ function App() {
             <Row>
               <Col>
                 <SongPlayer />
-                <ArtistInfo />
+                <Row>
+                  <Col xs="auto">
+                    <ArtistInfo />
+                  </Col>
+                  
+                    <YourRating />    
+                  
+                </Row>
+                
+                
               </Col>
 
               <Col xs="auto" className="d-none d-lg-inline">
---
+++ client/src/components/YourRating.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import { Col, Row } from 'reactstrap';
+
+class YourRating extends Component {
+    render() {
+        return(
+            <div className="text-white your-rating">
+                <p className="your-rating-h">Your Review: </p>
+                <textarea className="text-area" placeholder="What do you think about the song? (optional)"></textarea>
+                    <div className="review-buttons mt-3">
+                        <FontAwesomeIcon icon={['fas', 'thumbs-up']} className="mr-1 mr-3" size="lg" />
+                        <FontAwesomeIcon icon={['fas', 'thumbs-down']} className="mr-1 inactive" size="lg" />
+                            
+                    </div>
+            </div>
+            
+        );
+    }
+}
+
+export default YourRating;
